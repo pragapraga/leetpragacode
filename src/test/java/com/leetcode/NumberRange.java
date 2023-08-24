2a8f6ff9c582d1e7f427db1c8e07c2171249cf4a
pragapraga <pragadees24@gmail.com>
2023-08-25 00:45:27 +0530

    Committing Homework

--- src/test/java/com/leetcode/NumberRange.java
+++ src/test/java/com/leetcode/NumberRange.java
@@ -9,32 +9,31 @@ import java.util.List;
 
 public class NumberRange {
     public List<String> summaryRanges(int[] nums) {
-        ArrayList<String> al=new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
 
-        for(int i=0;i<nums.length;i++){
-            int start=nums[i];
-            while(i+1<nums.length && nums[i]+1==nums[i+1])
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            while (i+1 < nums.length && nums[i] + 1 == nums[i + 1])
                 i++;
 
-            if(start!=nums[i]){
-                al.add(""+start+"->"+nums[i]);
-            }
-            else{
-                al.add(""+start);
+            if (start == nums[i]) {
+                result.add("" + start);
+            } else {
+                result.add(start + "->" + nums[i]);
             }
         }
-        return al;
+        return result;
     }
 
     @Test
-    public void testme(){
-        int[] input = new int[]{0,1,2,4,5,7};
-        Assert.assertEquals(summaryRanges(input), Arrays.asList("0->2","4->5","7"));
+    public void testme() {
+        int[] input = new int[]{0, 1, 2, 4, 5, 7};
+        Assert.assertEquals(summaryRanges(input), Arrays.asList("0->2", "4->5", "7"));
     }
 
     @Test
-    public void testTwo(){
-        int[] input = new int[]{0,2,3,4,6,8,9};
-        Assert.assertEquals(summaryRanges(input), Arrays.asList("0","2->4","6","8->9"));
+    public void testTwo() {
+        int[] input = new int[]{0, 2, 3, 4, 6, 8, 9};
+        Assert.assertEquals(summaryRanges(input), Arrays.asList("0", "2->4", "6", "8->9"));
     }
 }
