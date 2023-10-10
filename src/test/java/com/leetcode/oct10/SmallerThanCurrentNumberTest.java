0f1350c552a502d1b686675574b2d2ad3e109005
pragapraga <praga@mac.local>
2023-10-10 08:22:13 +0530

    adding files and homework

--- src/test/java/com/leetcode/oct10/SmallerThanCurrentNumberTest.java
+++ src/test/java/com/leetcode/oct10/SmallerThanCurrentNumberTest.java
@@ -2,6 +2,23 @@ package com.leetcode.oct10;
 
 import java.util.Arrays;
 
+/*
+
+@input - int[]
+@output - int[]
+
+Testdata 1 -  nums = [6,5,4,8]  => [2,1,0,3]
+Testdata 2 -  nums = [7,7,7,7]  => [0,0,0,0]
+
+Brute force Psuedo code
+1. Have 2 for loops
+1.1 Outter for loop for overall iteration
+1.2 Inner for loop to compare each element other than itself
+2. Have a resultant int[] to store the values
+
+*/
+
+
 public class SmallerThanCurrentNumberTest {
     public static void main(String[] args) {
     SmallerThanCurrentNumberTest sm = new SmallerThanCurrentNumberTest();
